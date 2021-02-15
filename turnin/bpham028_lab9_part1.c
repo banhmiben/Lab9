b13328d840f70b19f1d564acf96009e1e5ba5082
Ben <benpham_12@yahoo.com>
2021-02-14 20:57:58 -0800

    Finished parts 1-4 with demo link

--- turnin/bpham028_lab9_part1.c
+++ turnin/bpham028_lab9_part1.c
@@ -1,11 +1,13 @@
 /*	Author: lab
  *  Partner(s) Name: 
  *	Lab Section:
- *	Assignment: Lab #  Exercise #
+ *	Assignment: Lab #9  Exercise #1
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
+ *
+ *	Demo Link: https://youtu.be/rCoShBmwaVk
  */
 
 #include <avr/io.h>
